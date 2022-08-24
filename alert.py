cf05a4821a2146d9768687b3d39f81bfe962f489
willywalshe <74630059+willywalshe@users.noreply.github.com>
2022-08-24 22:30:33 +0100

    Update alert.py

--- alert.py
+++ alert.py
@@ -3,7 +3,7 @@ import pandas_ta as ta
 import pandas as pd
 import requests
 import time
-def()
+def job()
     exchange = ccxt.binance()
 
     bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='5m', limit=500)
